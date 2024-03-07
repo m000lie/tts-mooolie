a2397250e0f00d3529daa8b55832a963a7405b99
Rainnen W <mooolie@icloud.com>
2024-03-06 19:54:17 -0500

    updated WD

--- TTS/demos/xtts_ft_demo/utils/gpt_train.py
+++ TTS/demos/xtts_ft_demo/utils/gpt_train.py
@@ -123,7 +123,7 @@ def train_gpt(language, num_epochs, batch_size, grad_acumm, train_csv, eval_csv,
         # Optimizer values like tortoise, pytorch implementation with modifications to not apply WD to non-weight parameters.
         optimizer="AdamW",
         optimizer_wd_only_on_weights=OPTIMIZER_WD_ONLY_ON_WEIGHTS,
-        optimizer_params={"betas": [0.9, 0.96], "eps": 1e-8, "weight_decay": 5e-3},
+        optimizer_params={"betas": [0.9, 0.96], "eps": 1e-8, "weight_decay": 5e-6},
         lr=5e-08,  # learning rate
         lr_scheduler="MultiStepLR",
         # it was adjusted accordly for the new step scheme
